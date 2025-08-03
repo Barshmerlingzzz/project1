ad11a487be95f6a844d2ed01fb6c3264ad6447f3
Bar <bar.shmerling@gmail.com>
2025-08-03 16:58:23 +0300

    pushing

---
+++ Shirt.java
@@ -0,0 +1,26 @@
+public class Shirt {
+    public int size;
+    public String color;
+
+    public Shirt(int size, String color) {
+        this.size = size;
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
