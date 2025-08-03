14010dd9beeb82c0ff83dfd56e7174ba16b80451
Bar <bar.shmerling@gmail.com>
2025-08-03 17:50:12 +0300

    56

---
+++ MeStudent.java
@@ -0,0 +1,12 @@
+public class MeStudent {
+
+    public static void createMe(){
+        Student bar = new Student(bar, 9, 4);
+
+        if (  bar.isStudentHS()){
+            System.out.println("bar is in hs");
+        }
+        System.out.println("bar is not in hs");
+        bar.toString(bar);
+    }
+}
--- Student.java
+++ Student.java
@@ -1,14 +1,18 @@
-public class Student {
-    public record student(String name, int grade, int classNum) {
+
+    public record Student(String name, int grade, int classNum) {
         public String toString() {
             return "student{" + "name='" + name + '\'' + ", grade=" + grade + ", classNum=" + classNum + '}';
         }
-        public boolean isStudentHS(){
-            if (student.this.grade >= 9){
+
+        public boolean isStudentHS() {
+            if (Student.this.grade >= 9) {
                 return true;
             }
             return false;
         }
 
+        public Student(String name, int classNum ){
+            this(name,9, classNum);
+        }
     }
 }
