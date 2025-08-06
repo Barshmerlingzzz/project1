92d390a74ca6ea256e973e6e878cb2103d1445f8
unknown <bar.shmerling@gmail.com>
2025-08-06 15:18:09 +0300

    all problems of records fixed

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int x = 5;
+       func(x);
+        System.out.println(x);
+    }
+    public static void func(int n){
+        n = 3;
+    }
+}
--- Student.java
+++ Student.java
@@ -1,18 +1,19 @@
 
     public record Student(String name, int grade, int classNum) {
+
+        private static final int GRADE_TET = 9;
+
+
         public String toString() {
             return "student{" + "name='" + name + '\'' + ", grade=" + grade + ", classNum=" + classNum + '}';
         }
 
         public boolean isStudentHS() {
-            if (Student.this.grade >= 9) {
-                return true;
-            }
-            return false;
+            return grade >= GRADE_TET;
         }
 
         public Student(String name, int classNum ){
-            this(name,9, classNum);
+            this(name,GRADE_TET, classNum);
         }
     }
 
