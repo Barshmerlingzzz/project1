8c2a8f49f33263ee51942e9b306cc6198e6cb8e1
unknown <bar.shmerling@gmail.com>
2025-08-06 19:31:19 +0300

    push

---
+++ Month.java
@@ -0,0 +1,12 @@
+public enum Month {
+    JENUARY("jenuary", 31);
+    FEBUARY("febuary", 28);
+    MARCH("march", 31);
+    APRIL()
+    MAY()
+    JUNE()
+    JYLY()
+    AUGUST()
+    SEPTEMBER()
+    OCTOBER()
+}
