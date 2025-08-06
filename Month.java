75fb1849f568519bbaf1a16afad325949c8e2350
unknown <bar.shmerling@gmail.com>
2025-08-06 20:06:49 +0300

    1-4

--- Month.java
+++ Month.java
@@ -1,12 +1,52 @@
 public enum Month {
-    JENUARY("jenuary", 31);
-    FEBUARY("febuary", 28);
-    MARCH("march", 31);
-    APRIL()
-    MAY()
-    JUNE()
-    JYLY()
-    AUGUST()
-    SEPTEMBER()
-    OCTOBER()
+    JENUARY(31),
+
+    FEBUARY(28),
+
+    MARCH(31),
+
+    APRIL(30),
+
+    MAY(31),
+
+    JUNE(30),
+
+    JYLY(31),
+
+    AUGUST(31),
+
+    SEPTEMBER(30),
+
+    OCTOBER(31),
+
+    NOVEMBER(30),
+
+    DECEMBER(31);
+
+    private String month;
+    private int days;
+
+    Month(int days) {
+        this.days = days;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    int monthNum(String month) {
+        return (ordinal() + 1);
+    }
+
+    public int numbsBefore(String month, int day) {
+        int sum = 0;
+        int ordi = monthNum(month);
+        for (int i = 0; i <; i++) {
+
+        }
+    }
 }
