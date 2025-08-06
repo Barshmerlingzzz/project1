709348fa3f4415189f876eb42d99bd06d2f6f9ab
unknown <bar.shmerling@gmail.com>
2025-08-06 16:50:30 +0300

    push

--- DoubleTimedValue.java
+++ DoubleTimedValue.java
@@ -2,4 +2,8 @@ public class DoubleTimedValue {
 private double value;
 private double time;
 
+    public DoubleTimedValue(double value, double time) {
+        this.value = value;
+        this.time = time;
+    }
 }
