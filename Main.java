8e7f9a02223f5ee12f7c9ac6dca8ed4017252acd
unknown <bar.shmerling@gmail.com>
2025-08-19 20:58:41 +0300

    push to add brance

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        public void ex1(){
-            Exception exception = new Exception();
-            RuntimeException runException = new RuntimeException("Exception found ");
 
-        }
 
 
     }
